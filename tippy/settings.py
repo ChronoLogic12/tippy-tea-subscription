a927095bed835b08f5a67bf1dd28f73ed5d1f276
ChronoLogic <msuttie12@hotmail.com>
2022-06-16 10:21:03 +0100

    Maint: Add django livereload configuration settings

--- tippy/settings.py
+++ tippy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'allauth',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'tippy.urls'
