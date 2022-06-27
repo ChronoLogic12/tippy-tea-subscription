207804ebe024581c85111b1a0efedf4dc3b23a07
ChronoLogic <msuttie12@hotmail.com>
2022-06-27 22:58:10 +0100

    Reaf: refactor background styling elements

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.CharField(max_length=10000, null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
