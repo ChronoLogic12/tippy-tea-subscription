474fcfd125b418ce9850860be09ac0d8ca043bea
ChronoLogic <msuttie12@hotmail.com>
2022-06-29 11:46:08 +0100

    Test: Create view tests for blog page

---
+++ blog/tests/test_view.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class TestAboutView(TestCase):
+
+    def test_url_exists_at_desired_location(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        response = self.client.get(reverse('blog'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('blog'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog.html')
