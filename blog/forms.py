553a460299218535eaa1e13efa6563853ad78c42
ChronoLogic <msuttie12@hotmail.com>
2022-07-05 23:48:46 +0100

    Style: change the formatting of blog content form input to user materialize text area styling. Remove visable lables and replace with placeholder text for blog post forms.

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+from tkinter import Widget
 from django import forms
 from .models import Blog
 
@@ -7,3 +8,28 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = '__all__'
+        widgets = {
+            'content': forms.Textarea,
+        }
+
+    def __init__(self, *args, **kwargs):
+            """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+            """
+            super().__init__(*args, **kwargs)
+            placeholders = {
+                'title': 'Title',
+                'author': 'Author',
+                'content': 'Content',
+                'image_url': 'Image URL',
+            }
+
+            self.fields['title'].widget.attrs['autofocus'] = True
+            for field in self.fields:
+                if field == "content":
+            
+                    self.fields["content"].widget.attrs['class'] = 'materialize-textarea'
+                placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].label = False
