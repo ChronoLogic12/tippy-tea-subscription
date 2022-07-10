9e94220612d25763617fa039be3acef18407243a
ChronoLogic <msuttie12@hotmail.com>
2022-07-10 19:09:03 +0100

    Feat: create models for profile app

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,36 @@
+from cProfile import label
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.forms import BooleanField
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=80, null=True, blank=True)
+    email = models.EmailField(max_length=254)
+    created_on = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country', null=True, blank=True)
+    mailing = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
