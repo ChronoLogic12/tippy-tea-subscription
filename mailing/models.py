9165729b3255915dd1012ede47b6986ce99fa722
ChronoLogic <msuttie12@hotmail.com>
2022-07-11 14:23:41 +0100

    Feat: create model for mailing app

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Mailing(models.Model):
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
