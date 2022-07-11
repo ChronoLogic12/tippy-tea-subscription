147b4f9f11092b3affc26781c4ce529826fe786f
ChronoLogic <msuttie12@hotmail.com>
2022-07-11 19:53:44 +0100

    Maint: remove user field from mailing list model

--- mailing/models.py
+++ mailing/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Mailing(models.Model):
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    email = models.EmailField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.email
