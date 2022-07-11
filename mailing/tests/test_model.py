2f682589173cf7f81235055a1634496fd8256775
ChronoLogic <msuttie12@hotmail.com>
2022-07-11 14:09:39 +0100

    Test: creat model tests for mailing app

---
+++ mailing/tests/test_model.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from mailing.models import Mailing
+from django.utils.crypto import get_random_string
+from django.contrib.auth.models import User
+
+
+class TestMailingModel(TestCase):
+    def setUp(self):
+        string = get_random_string(20)
+        self.test_email = 'test@example.com'
+        self.user = User.objects.create_user(username='test_user', password='testpassword')
+        self.client.login(username='test_user', password='testpassword')
+        self.email = Mailing.objects.create(email=f'{string}@example.com', user=self.user)
+        
+    def test_email_added(self):
+        self.assertEqual(len(Mailing.objects.all()), 1)
+        return_email = Mailing.objects.get(email=self.email)
+        self.assertEqual(return_email, self.email)
+    
+    def test_user_added(self):
+        return_email = Mailing.objects.get(user=self.user)
+        self.assertEqual(return_email.user, self.user)
+
