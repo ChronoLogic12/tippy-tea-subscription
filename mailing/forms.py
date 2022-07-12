14ded2ed13eabde9f05fc1015d84b4982555de5c
ChronoLogic <msuttie12@hotmail.com>
2022-07-12 10:17:23 +0100

    Feat: create form file for newsetter emails

--- mailing/forms.py
+++ mailing/forms.py
@@ -21,3 +21,27 @@ class MailingForm(forms.ModelForm):
         self.fields['email'].widget.attrs['required'] = True
         self.fields['email'].widget.attrs['placeholder'] = 'Email Address'
         self.fields['email'].label = False
+
+
+class MassEmailForm(forms.Form):
+    subject = forms.CharField(max_length=180 )
+    message = forms.CharField(max_length=10000, widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+            """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+            """
+            super().__init__(*args, **kwargs)
+            placeholders = {
+                'subject': 'Subject',
+                'message': 'Message',
+            }
+
+            self.fields['subject'].widget.attrs['autofocus'] = True
+            self.fields["message"].widget.attrs['class'] = 'materialize-textarea'
+            for field in self.fields:
+                placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].label = False
+
