953a2ff5dc12a550047138b3eddd8c16a596e76a
ChronoLogic <msuttie12@hotmail.com>
2022-07-18 21:06:13 +0100

    Fix: remove logging setting. deployment bug fix attempt

--- tippy/settings.py
+++ tippy/settings.py
@@ -223,13 +223,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-LOGGING = {
-    'handlers': {
-        'console':{
-            'level':'INFO',
-            'class':'logging.StreamHandler',
-            'strm': sys.stdout
-        },
-    }
-}
