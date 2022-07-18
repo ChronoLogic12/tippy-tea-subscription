e2ca7718a8290d092bb98d858ca4aa7bffddedcb
ChronoLogic <msuttie12@hotmail.com>
2022-07-18 13:23:42 +0100

    Maint: update model and form field name for customer_id

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,7 +7,7 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields='__all__'
-        exclude = ('user', 'checkout_session_id')
+        exclude = ('user', 'customer_id')
         
 
     def __init__(self, fields_required=False, *args, **kwargs):
