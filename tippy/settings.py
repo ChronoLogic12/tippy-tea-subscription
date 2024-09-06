74f99bd77e112e82d64a6f47dffbe84de5f7ac68
ChronoLogic <msuttie12@hotmail.com>
2024-09-07 00:50:06 +0100

    Add static_root to settings.py

--- tippy/settings.py
+++ tippy/settings.py
@@ -163,6 +163,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# Define the path for static files collection
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
