ee7051f7c76c6176db649715cf59b42503b17cca
ChronoLogic <msuttie12@hotmail.com>
2024-09-10 20:48:20 +0100

    Add EMAIL_USE_SSL value to settings

--- tippy/settings.py
+++ tippy/settings.py
@@ -205,6 +205,7 @@ if not 'USE_AWS' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
